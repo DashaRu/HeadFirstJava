d15080133811a980bc68ad6d5ab5316bed640532
DashaRu <daluca@mail.ru>
2020-11-02 19:15:15 +0300

    Initial commit

---
+++ src/Chapter1/IfTest.java
@@ -0,0 +1,11 @@
+package Chapter1;
+
+public class IfTest {
+    public static void main (String[] args) {
+        int x = 3;
+        if (x == 3) {
+            System.out.println("х должен равняться 3");
+        }
+        System.out.println("Эта строка выполняется в любом случае");
+    }
+}
---
+++ src/Chapter1/MyFirstApp.java
@@ -0,0 +1,9 @@
+package Chapter1;
+
+public class MyFirstApp {
+
+    public static void main (String[] args) {
+        System.out.println("Я управляю");
+        System.out.println("миром");
+    }
+}
